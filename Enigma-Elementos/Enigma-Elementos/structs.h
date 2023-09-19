1fadd522f4aea0604ab6d8b3268a78abf4c2478f
fillipevs <fillipevital@hotmail.com>
2023-09-18 21:56:39 -0300

    Organização de arquivos
    Realizei a organização dos arquivos e a criação de um projeto chamado Enigma-Elementos para execução no Visual estudio.
    
    Adicionado também o pacote Allegro as pastas do Git

---
+++ Enigma-Elementos/Enigma-Elementos/structs.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+#include <allegro5/keyboard.h> 
+#include <allegro5/allegro_image.h> 
+
+typedef struct {
+	int posX;
+	int posY;
+	int vel;
+	bool indoCima;
+	bool indoDireita;
+	bool indoBaixo;
+	bool indoEsquerda;
+	float frame;
+	int frameMax;
+	int frameAtualY;
+	ALLEGRO_BITMAP* sprite; // 141 / 3 = 47 -> largura; 194 / 4 = 48.5 -> altura;
+} Personagem;
+
+Personagem protagonista = { 0, 0, 1, false, false, false, false, 0.f, 3, 0 };
